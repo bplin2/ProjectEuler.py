55b4ae8d586cb7b892e6b955d42dea35242732d1
Brian Lin <lin.p.brian@gmail.com>
2017-05-24 13:01:27 -0500

    Added comments for 6.py

--- 6.py
+++ 6.py
@@ -1,6 +1,10 @@
 sumsquare = sum(n ** 2 for n in range(1,101))
+
+#Sum of all numbers from 1 to n is equal to (n*(n+1))/2
 squaresum = ((100*(100+1))/2) ** 2
 
 ans = squaresum - sumsquare
 
 print ans
+
+#25164150
